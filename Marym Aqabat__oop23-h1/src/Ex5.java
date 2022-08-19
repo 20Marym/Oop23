d7167e7f57d10409c953e7b63b7bc00e492c083d
20Marym <111463613+20Marym@users.noreply.github.com>
2022-08-20 00:20:25 +0300

    Add files via upload

---
+++ Marym Aqabat__oop23-h1/src/Ex5.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        System.out.println("* * * * * * *");
+        System.out.println("*\t\t\t*");
+        System.out.println("*\t\t\t*");
+        System.out.println("*\tMARYM\t*");
+        System.out.println("*\t\t\t*");
+        System.out.println("*\t\t\t*");
+        System.out.println("* * * * * * *");
+    }
+}
